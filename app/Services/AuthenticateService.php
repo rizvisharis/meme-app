bfb19ab5353437956034702dbc9b200ce5e52382
Rizvi <rizvisharis@gmail.com>
2021-09-26 10:50:39 +0530

    login method code changed

--- app/Services/AuthenticateService.php
+++ app/Services/AuthenticateService.php
@@ -2,25 +2,21 @@
 
 namespace App\Services;
 
-use App\Models\User;
 use App\Services\Interfaces\AuthenticateServiceInterface;
 use App\Utils\Constants;
 use Exception;
-use Illuminate\Support\Facades\Hash;
 
 class AuthenticateService implements AuthenticateServiceInterface
 {
     public function login(array $requestData)
     {
         try {
-            $user = User::where('email', $requestData['email'])->first();
-            if (!$user || !Hash::check($requestData['password'], $user->password))
-                throw new Exception(
-                    Constants::$ERROR_MESSAGE['no_email_password'], Constants::$ERROR_CODE['not_found']);
+            if (!auth()->attempt($requestData)) throw new Exception(
+                Constants::$ERROR_MESSAGE['no_email_password'], Constants::$ERROR_CODE['not_found']);
 
             return [
-                'token' => $user->createToken($user->first_name)->plainTextToken,
-                'user' => $user
+                'token' => auth()->user()->createToken(auth()->user()->first_name)->plainTextToken,
+                'user' => auth()->user()
             ];
         } catch (Exception $exception) {
             throw $exception;
